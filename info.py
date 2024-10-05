18f6e43b40f6022622cc8afb78ed07106e324888
Jkmovie <124867430+Jkmovie@users.noreply.github.com>
2024-10-06 00:43:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://t.me/+yq7gGZq1Ur9mMjQ9")
+URL = environ.get("URL", "https://t.me/bingjkk")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
